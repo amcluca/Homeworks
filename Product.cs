89dac64e11a12bb538121b4e04ddff7aaea1baf8
amcluca <71344613+amcluca@users.noreply.github.com>
2020-10-06 21:04:13 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product
+{
+    class Product
+    {
+        private ProductWeight weight;
+        private ProductName name;
+        private ProductProducer producer;
+        private ProductCategory category;
+        private DateTime productionDate;
+        private DateTime expirationDate;
+
+        public Product(ProductWeight weight, ProductName name, DateTime productionDate, 
+            DateTime expirationDate, ProductProducer producer, ProductCategory category)
+        {
+            this.weight = weight;
+            this.name = name;
+            this.productionDate = productionDate;
+            this.expirationDate = expirationDate;
+            this.producer = producer;
+            this.category = category;
+
+        }
+
+
+        public ProductWeight Weight
+        {
+            get { return weight; }
+        }
+
+        public ProductName Name
+        {
+            get { return name; }
+        }
+
+        public string ProductionDate1
+        {
+            get
+            {
+                return productionDate.Year.ToString();
+            }
+        }
+
+        public string ProductionDate2
+        {
+            get
+            {
+                return productionDate.Year.ToString();
+            }
+        }
+
+        public string ProductionDate3
+        {
+            get
+            {
+                return productionDate.Year.ToString();
+            }
+        }
+
+        public string ProductionDate4
+        {
+            get
+            {
+                return productionDate.Year.ToString();
+            }
+        }
+
+        public string ExpirationDate1
+        {
+            get
+            {
+                return expirationDate.Year.ToString();
+            }
+        }
+
+        public string ExpirationDate2
+        {
+            get
+            {
+                return expirationDate.Year.ToString();
+            }
+        }
+
+        public string ExpirationDate3
+        {
+            get
+            {
+                return expirationDate.Year.ToString();
+            }
+        }
+
+        public string ExpirationDate4
+        {
+            get
+            {
+                return expirationDate.Year.ToString();
+            }
+        }
+
+
+
+        public ProductProducer Producer
+        {
+            get { return producer; }
+        }
+
+        public ProductCategory Category
+        {
+            get { return category; }
+        }
+
+
+    }
+
+    public enum ProductName
+    {
+        Milk,
+        Apple,
+        Wine,
+        Bread
+    }
+
+    public enum ProductWeight
+    {
+        one_liter,
+        one_kilo,
+        one_bottle,
+    }
+
+    public enum ProductProducer
+    {
+        Zuzu,
+        Golden,
+        Cotnari,
+        Panifcom
+    }
+
+    public enum ProductCategory
+    {
+        Dairy, 
+        Fruits, 
+        Beverages, 
+        Bread
+    }
+}    
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            DateTime productionDate1 = new DateTime(2020, 10, 04);
+            DateTime productionDate2 = new DateTime(2020, 10, 04);
+            DateTime productionDate3 = new DateTime(2020, 10, 04);
+            DateTime productionDate4 = new DateTime(2020, 10, 04);
+            DateTime expirationDate1 = new DateTime(2020, 10, 11);
+            DateTime expirationDate2 = new DateTime(2020, 10, 06);
+            DateTime expirationDate3 = new DateTime(2023, 10, 04);
+            DateTime expirationDate4 = new DateTime(2020, 10, 08);
+            Product product1 = new Product(ProductWeight.one_liter, ProductName.Milk, productionDate1, 
+                                          expirationDate1, ProductProducer.Zuzu, ProductCategory.Dairy);
+            Product product2 = new Product(ProductWeight.one_kilo, ProductName.Apple, productionDate2, 
+                                          expirationDate2, ProductProducer.Golden, ProductCategory.Fruits);
+            Product product3 = new Product(ProductWeight.one_liter, ProductName.Wine, productionDate3, 
+                                       expirationDate3, ProductProducer.Cotnari, ProductCategory.Beverages);
+            Product product4 = new Product(ProductWeight.one_kilo, ProductName.Bread, productionDate4, 
+                                          expirationDate4, ProductProducer.Panifcom, ProductCategory.Bread);
+
+            Console.WriteLine("Product Number One has: " + product1.Weight);
+            Console.WriteLine("Product Number One is: " + product1.Name);
+            Console.WriteLine("Production date of Product Number One is: " + productionDate1.ToString("MM/dd/yyyy"));
+            Console.WriteLine("Expiration date of Product Number One is: " + expirationDate1.ToString("MM/dd/yyyy"));
+            Console.WriteLine("Producer of Product Number One is: " + product1.Producer);
+            Console.WriteLine("");
+
+            Console.WriteLine("Product Number Two has: " + product2.Weight);
+            Console.WriteLine("Product Number Two is: " + product2.Name);
+            Console.WriteLine("Production date of Product Number Two is: " + productionDate2.ToString("MM/dd/yyyy"));
+            Console.WriteLine("Expiration date of Product Number Two is: " + expirationDate2.ToString("MM/dd/yyyy"));
+            Console.WriteLine("Producer of Product Number Two is: " + product2.Producer);
+            Console.WriteLine("");
+
+            Console.WriteLine("Product Number Three has: " + product3.Weight);
+            Console.WriteLine("Product Number Three is: " + product3.Name);
+            Console.WriteLine("Production date of Product Number Three is: " + productionDate3.ToString("MM/dd/yyyy"));
+            Console.WriteLine("Expiration date of Product Number Three is: " + expirationDate3.ToString("MM/dd/yyyy"));
+            Console.WriteLine("Producer of Product Number Three is: " + product3.Producer);
+            Console.WriteLine("");
+
+            Console.WriteLine("Product Number Four has: " + product4.Weight);
+            Console.WriteLine("Product Number Four is: " + product4.Name);
+            Console.WriteLine("Production date of Product Number Four is: " + productionDate4.ToString("MM/dd/yyyy"));
+            Console.WriteLine("Expiration date of Product Number Four is: " + expirationDate4.ToString("MM/dd/yyyy"));
+            Console.WriteLine("Producer of Product Number Four is: " + product4.Producer);
+            Console.WriteLine("");
+
+            Console.ReadLine();
+        }
+    }
+}
